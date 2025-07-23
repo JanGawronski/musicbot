80194efedcf2d5d7504951cbe6ecfd0ae84bd2b0
JanGawronski <jangawronski04@gmail.com>
2025-07-23 12:22:32 +0200

    Added test slash command

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
+mod commands;
+
 use std::env;
 use dotenv::dotenv;
 
 use serenity::async_trait;
-use serenity::model::channel::Message;
+use serenity::builder::{CreateInteractionResponse, CreateInteractionResponseMessage};
+use serenity::model::application::Interaction;
+use serenity::model::gateway::Ready;
+use serenity::model::id::GuildId;
 use serenity::prelude::*;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {why:?}");
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::Command(command) = interaction {
+            println!("Received command interaction: {command:#?}");
+
+            let content = match command.data.name.as_str() {
+                "ping" => Some(commands::ping::run(&command.data.options())),
+                _ => Some("not implemented".to_string()),
+            };
+
+            if let Some(content) = content {
+                let data = CreateInteractionResponseMessage::new().content(content);
+                let builder = CreateInteractionResponse::Message(data);
+                if let Err(why) = command.create_response(&ctx.http, builder).await {
+                    println!("Cannot respond to slash command: {why}");
+                }
             }
         }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId::new(
+            env::var("GUILD_ID")
+                .expect("Expected GUILD_ID in environment")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+
+        let commands = guild_id 
+            .set_commands(&ctx.http, vec![
+                commands::ping::register(),
+            ])
+            .await;
+
+        println!("I now have the following guild slash commands: {commands:#?}");
+
     }
 }
 
@@ -22,11 +58,9 @@ impl EventHandler for Handler {
 async fn main() {
     dotenv().ok();
 
-    let token = env::var("DISCORD_TOKEN").expect("Variable DISCORD_TOKEN should be set");
+    let token = env::var("DISCORD_TOKEN").expect("env variable `DISCORD_TOKEN` should be set by `.env` file");
 
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::GUILDS | GatewayIntents::GUILD_VOICE_STATES;
 
     let mut client =
         Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
