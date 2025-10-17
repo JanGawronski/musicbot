a201bf3d4c435aed0150327cdaffcbe65d22ee2b
JanGawronski <jangawronski04@gmail.com>
2025-10-17 02:08:49 +0200

    Added gathering metadata from local files

--- src/utils/local_files.rs
+++ src/utils/local_files.rs
@@ -1,6 +1,17 @@
 use std::{
     fs::read_dir,
+    path::PathBuf,
     collections::HashMap,
+};
+
+use lofty::{
+    probe::Probe,
+    tag::Accessor,
+    file::{
+        TaggedFileExt,
+        TaggedFile,
+        AudioFile,
+    },
 };
 
 use super::audio::Metadata;
@@ -15,25 +26,41 @@ pub fn get_audio_files() -> HashMap<String, Metadata> {
     };
 
     paths.filter_map(|entry| {
-        if let Ok(entry) = entry {
-            let path = entry.path();
-            if path.is_file() {
-                if let Some(filename) = path.file_name().and_then(|n| n.to_str()) {
-                    let metadata = Metadata {
-                        title: Some(filename.to_string()),
-                        uploader: None,
-                        track: None,
-                        artist: None,
-                        duration: None,
-                        thumbnail: None,
-                        webpage_url: None,
-                        url: Some(path.to_string_lossy().to_string()),
+        let entry = entry.ok()?;
+        let path = entry.path();
+        if path.is_file() {
+            let filename = path.file_name().and_then(|n| n.to_str())?.to_string();
+            let tagged_file = get_tagged_file(&path);
+            let tag = tagged_file.as_ref().and_then(|f| f.primary_tag())
+                .or(tagged_file.as_ref().and_then(|f| f.first_tag()));
 
-                    };
-                    return Some((filename.to_string(), metadata));
-                }
-            } 
+            let title = tag.and_then(|t| t.title().map(|s| s.to_string()));
+            let artist = tag.and_then(|t| t.artist().map(|s| s.to_string()));
+
+            let duration = tagged_file.as_ref()
+                .and_then(|f| f.properties().duration().as_secs().try_into().ok());
+
+            Some((title.clone().unwrap_or(filename.clone()), Metadata {
+                title: Some(filename),
+                uploader: None,
+                track: title,
+                artist: artist,
+                duration: duration,
+                thumbnail: None,
+                webpage_url: None,
+                url: path.to_str().map(|s| s.to_string()),
+                }))
+        } else {
+            None
         }
-        None
     }).collect()
-} 
+}
+
+fn get_tagged_file(path: &PathBuf) -> Option<TaggedFile> {
+    Probe::open(path)
+        .ok()?
+        .guess_file_type()
+        .ok()?
+        .read()
+        .ok()
+}
